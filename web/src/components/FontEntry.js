84a81eb69061c99d71c8ea8eef765bddf7a786b6
Kyle Melton <kalm42@gmail.com>
2019-10-27 18:38:49 -0600

    feat: Display fonts

---
+++ web/src/components/FontEntry.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { Helmet } from 'react-helmet'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+
+const Wrapper = styled.div`
+  border-top: 1px solid black;
+  padding: 15px;
+`
+const Header = styled.div`
+  display: flex;
+`
+const Details = styled.div`
+  flex: 1;
+`
+const AddFontBtn = styled.button`
+  position: relative;
+  top: 0;
+  right: 0;
+  height: 40px;
+  width: 40px;
+  border-radius: 50%;
+  border: none;
+  background: var(--red);
+  color: white;
+`
+const FontExample = styled.p`
+  font-family: ${props => `'${props.font.fontFamily}', ${props.font.taxonomy}`};
+  font-size: ${props => `${props.font.size}px`};
+`
+
+const FontEntry = ({ font }) => {
+  return (
+    <Wrapper>
+      <Helmet>
+        <link rel="stylesheet" href={font.css} />
+      </Helmet>
+      <Header>
+        <Details>
+          <p>{font.name}</p>
+          <p>
+            {font.foundry} ({font.styles} styles)
+          </p>
+        </Details>
+        <AddFontBtn type="button">
+          <FontAwesomeIcon icon={faPlus} />
+        </AddFontBtn>
+      </Header>
+      <FontExample font={font}>
+        Consequat nostrud amet duis ullamco enim Lorem.
+      </FontExample>
+    </Wrapper>
+  )
+}
+
+FontEntry.propTypes = {
+  font: PropTypes.shape({
+    id: PropTypes.string,
+    name: PropTypes.string,
+    styles: PropTypes.number,
+    foundry: PropTypes.string,
+    fontFamily: PropTypes.string,
+    taxonomy: PropTypes.string,
+    source: PropTypes.string,
+    css: PropTypes.string,
+  }).isRequired,
+}
+
+export default FontEntry
--- web/src/pages/Home.js
+++ web/src/pages/Home.js
@@ -6,6 +6,7 @@ import PreviewTextSettingControl from '../components/PreviewTextSettingControl'
 import FontSearch from '../components/FontSearch'
 import SetPreviewText from '../components/SetPreviewText'
 import SetFontSize from '../components/SetFontSize'
+import FontEntry from '../components/FontEntry'
 
 const ControlBar = styled.div`
   display: flex;
@@ -16,6 +17,95 @@ const BlankButton = styled.button`
   background: none;
   border: none;
 `
+const FontCatalog = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+`
+
+const FakeFontData = [
+  {
+    id: '1sadcadckm',
+    name: 'Fira Code',
+    styles: 2,
+    foundry: 'Grazil Ltd',
+    fontFamily: 'Fira Code',
+    taxonomy: 'monospace',
+    source: 'https://dank.sh',
+    css: 'https://fonts.googleapis.com/css?family=Fira+Code&display=swap',
+  },
+  {
+    id: '2sadcadckm',
+    name: 'Fira Sans',
+    styles: 2,
+    foundry: 'Grazil Ltd',
+    fontFamily: 'Fira Sans',
+    taxonomy: 'sans-serif',
+    source: 'https://fonts.google.com/specimen/Roboto',
+    css: 'https://fonts.googleapis.com/css?family=Fira+Sans&display=swap',
+  },
+  {
+    id: '3sadcadckm',
+    name: 'Lato',
+    styles: 10,
+    foundry: 'Lukasz Dziedzic',
+    fontFamily: 'Lato',
+    taxonomy: 'sans-serif',
+    source: 'https://fonts.google.com/specimen/lato',
+    css: 'https://fonts.googleapis.com/css?family=Lato&display=swap',
+  },
+  {
+    id: '4sadcadckm',
+    name: 'Liu Jian Mao Cao',
+    styles: 1,
+    foundry: 'Liu Zhengjiang',
+    fontFamily: 'Liu Jian Mao Cao',
+    taxonomy: 'cursive',
+    source: 'https://fonts.google.com/specimen/lato',
+    css:
+      'https://fonts.googleapis.com/css?family=Liu+Jian+Mao+Cao&display=swap',
+  },
+  {
+    id: '5sadcadckm',
+    name: 'Nunito',
+    styles: 2,
+    foundry: 'Grazil Ltd',
+    fontFamily: 'Nunito',
+    taxonomy: 'sans-serif',
+    source: 'https://dank.sh',
+    css: 'https://fonts.googleapis.com/css?family=Nunito&display=swap',
+  },
+  {
+    id: '6sadcadckm',
+    name: 'Roboto',
+    styles: 12,
+    foundry: 'Christian Robertson',
+    fontFamily: 'Roboto',
+    taxonomy: 'sans-serif',
+    source: 'https://fonts.google.com/specimen/Roboto',
+    css: 'https://fonts.googleapis.com/css?family=Roboto&display=swap',
+  },
+  {
+    id: '7sadcadckm',
+    name: 'Roboto Slab',
+    styles: 10,
+    foundry: 'Lukasz Dziedzic',
+    fontFamily: 'Roboto Slab',
+    taxonomy: 'serif',
+    source: 'https://fonts.google.com/specimen/lato',
+    css: 'https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap',
+  },
+  {
+    id: '8sadcadckm',
+    name: 'Slabo 27px',
+    styles: 1,
+    foundry: 'Liu Zhengjiang',
+    fontFamily: 'Slabo 27px',
+    taxonomy: 'sans-serif',
+    source: 'https://fonts.google.com/specimen/lato',
+    css: 'https://fonts.googleapis.com/css?family=Slabo+27px&display=swap',
+  },
+]
 
 const Home = () => {
   const [loremDisplay, setLoremDisplay] = useState('Sentence')
@@ -68,129 +158,12 @@ const Home = () => {
         <option value="newest">Newest</option>
         <option value="name">Name</option>
       </select>
-      <div>
-        <h1>Font Catalog</h1>
-        <div>
-          <div>
-            <div>
-              <p>NAME</p>
-              <p>Author (10 styles)</p>
-            </div>
-            <button type="button">+</button>
-          </div>
-          <p>Consequat nostrud amet duis ullamco enim Lorem.</p>
-        </div>
-        <div>
-          <div>
-            <div>
-              <p>NAME</p>
-              <p>Author (10 styles)</p>
-            </div>
-            <button type="button">+</button>
-          </div>
-          <p>Consequat nostrud amet duis ullamco enim Lorem.</p>
-        </div>
-        <div>
-          <div>
-            <div>
-              <p>NAME</p>
-              <p>Author (10 styles)</p>
-            </div>
-            <button type="button">+</button>
-          </div>
-          <p>Consequat nostrud amet duis ullamco enim Lorem.</p>
-        </div>
-        <div>
-          <div>
-            <div>
-              <p>NAME</p>
-              <p>Author (10 styles)</p>
-            </div>
-            <button type="button">+</button>
-          </div>
-          <p>Consequat nostrud amet duis ullamco enim Lorem.</p>
-        </div>
-        <div>
-          <div>
-            <div>
-              <p>NAME</p>
-              <p>Author (10 styles)</p>
-            </div>
-            <button type="button">+</button>
-          </div>
-          <p>Consequat nostrud amet duis ullamco enim Lorem.</p>
-        </div>
-        <div>
-          <div>
-            <div>
-              <p>NAME</p>
-              <p>Author (10 styles)</p>
-            </div>
-            <button type="button">+</button>
-          </div>
-          <p>Consequat nostrud amet duis ullamco enim Lorem.</p>
-        </div>
-        <div>
-          <div>
-            <div>
-              <p>NAME</p>
-              <p>Author (10 styles)</p>
-            </div>
-            <button type="button">+</button>
-          </div>
-          <p>Consequat nostrud amet duis ullamco enim Lorem.</p>
-        </div>
-        <div>
-          <div>
-            <div>
-              <p>NAME</p>
-              <p>Author (10 styles)</p>
-            </div>
-            <button type="button">+</button>
-          </div>
-          <p>Consequat nostrud amet duis ullamco enim Lorem.</p>
-        </div>
-        <div>
-          <div>
-            <div>
-              <p>NAME</p>
-              <p>Author (10 styles)</p>
-            </div>
-            <button type="button">+</button>
-          </div>
-          <p>Consequat nostrud amet duis ullamco enim Lorem.</p>
-        </div>
-        <div>
-          <div>
-            <div>
-              <p>NAME</p>
-              <p>Author (10 styles)</p>
-            </div>
-            <button type="button">+</button>
-          </div>
-          <p>Consequat nostrud amet duis ullamco enim Lorem.</p>
-        </div>
-        <div>
-          <div>
-            <div>
-              <p>NAME</p>
-              <p>Author (10 styles)</p>
-            </div>
-            <button type="button">+</button>
-          </div>
-          <p>Consequat nostrud amet duis ullamco enim Lorem.</p>
-        </div>
-        <div>
-          <div>
-            <div>
-              <p>NAME</p>
-              <p>Author (10 styles)</p>
-            </div>
-            <button type="button">+</button>
-          </div>
-          <p>Consequat nostrud amet duis ullamco enim Lorem.</p>
-        </div>
-      </div>
+      <h1>Font Catalog</h1>
+      <FontCatalog>
+        {FakeFontData.map(font => (
+          <FontEntry key={font.id} font={{ ...font, size: fontSize }} />
+        ))}
+      </FontCatalog>
     </div>
   )
 }
